9a45f99c0467385d6d4878092d5b659000eb0369
Тимофей Тимофеев <pro100tim26rus@gmail.com>
2023-02-15 16:04:19 +0300

    Начало работы над календарем. Использован плагин.

---
+++ Note/Note/View/Calendar/CalendarPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Note.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Plugin.Calendar.Models;
+
+namespace Note.View
+{ 
+	public partial class CalendarPage : ContentPage
+	{
+		public CalendarPage()
+		{
+			InitializeComponent();
+            var date = DateTime.Parse("02.20.2023");
+			CalendarTest.ShownDate = DateTime.Now;
+            CalendarTest.EventIndicatorColor = Color.Purple;
+            CalendarTest.Events = new EventCollection
+            {
+                [date] = new List<NoteModel>
+                {
+                    new NoteModel { Title = "Cool event1", Text = "This is Cool event1's description!" },
+                    new NoteModel { Title = "Cool event2", Text = "This is Cool event2's description!" }
+                },
+                [DateTime.Now.AddDays(-1)] = new List<NoteModel>
+                {
+                    new NoteModel { Title = "Cool event1", Text = "This is Cool event1's description!" },
+                    new NoteModel { Title = "Cool event2", Text = "This is Cool event2's description!" }
+                },
+                [DateTime.Now.AddDays(1)] = new List<NoteModel>
+                {
+                    new NoteModel { Title = "Cool event1", Text = "This is Cool event1's description!" },
+                    new NoteModel { Title = "Cool event2", Text = "This is Cool event2's description!" }
+                }
+            };
+        }
+    }
+}
